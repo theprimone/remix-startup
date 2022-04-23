240a920878dd07643a1c4d9e088a2f9889261c81
yuns <yuns.xie@qq.com>
2022-04-23 11:43:14 +0800

    chore: update remix-run

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,8 @@
 /**
- * @type {import('@remix-run/dev/config').AppConfig}
+ * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
-  browserBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8113
+  cacheDirectory: './node_modules/.cache/remix',
+  ignoredRouteFiles: ['.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
 };
+// ref: https://remix.run/docs/en/v1/api/conventions
